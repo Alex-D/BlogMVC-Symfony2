07c87aaf9f5c3f54d0f2295f86d7283991018aee
Alex-D <contact@alex-d.fr>
2017-01-11 14:32:37 +0100

    init repository, moved from Grafikart/BlogMVC
    https://github.com/Grafikart/BlogMVC/issues/72#issuecomment-271855527

---
+++ src/Acme/BlogBundle/AcmeBlogBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Acme\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class AcmeBlogBundle
+ * @package Acme\BlogBundle
+ */
+class AcmeBlogBundle extends Bundle
+{
+}
---
+++ src/Acme/UserBundle/AcmeUserBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Acme\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class AcmeUserBundle
+ * @package Acme\UserBundle
+ */
+class AcmeUserBundle extends Bundle
+{
+}
