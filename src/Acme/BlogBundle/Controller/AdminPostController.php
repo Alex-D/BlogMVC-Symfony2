78e63ada70c55a6813863e0f8d12a15965e1d4d5
oussaka <oussaka@users.noreply.github.com>
2017-01-19 20:17:16 +0100

    remove unused condition

--- src/Acme/BlogBundle/Controller/AdminPostController.php
+++ src/Acme/BlogBundle/Controller/AdminPostController.php
@@ -69,9 +69,7 @@ class AdminPostController extends AbstractController
                 $em->flush();
 
                 // New post : update post count
-                if ($post->getId() !== null) {
-                    $this->clearCache();
-                }
+                $this->clearCache();
 
                 return $this->redirectAdmin();
             }
